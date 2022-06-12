f6ff4c89b39d1d9e75649fd425991ccb879c7879
YanivWein24 <yanivwein22@gmail.com>
2022-06-13 00:21:52 +0300

    Added more animations

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Joke = (props) => {
     return (
-        <div className="joke">
+        <div className="joke fade-in">
             <div className="jokeCategory">Category: {props.category}</div>
             <div>{props.joke}</div>
         </div>
@@ -11,7 +11,7 @@ const Joke = (props) => {
 
 const TwoPartJoke = (props) => {
     return (
-        <div className="joke">
+        <div className="joke fade-in">
             <div className="jokeCategory underline">Category: {props.category}</div>
             <div> {props.setup}</div>
             <div> {props.delivery}</div>
